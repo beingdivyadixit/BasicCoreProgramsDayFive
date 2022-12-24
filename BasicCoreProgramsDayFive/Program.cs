5d925c1c2b7174a7985d11fd81586519e977ec56
Divya <beingdivyadixit@gmail.com>
2022-12-24 18:02:19 +0530

    Leap Year Program

--- BasicCoreProgramsDayFive/Program.cs
+++ BasicCoreProgramsDayFive/Program.cs
@@ -7,9 +7,27 @@ namespace BasicCoreProgramsDayFive
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Basic CorePrograms"); //Welcome message
-            Console.WriteLine("Enter the Number of times you wanted to flip the coin"); //  Taking User Input For number of times wanted to flip coin
-            int N = Convert.ToInt32(Console.ReadLine());
-            flipCoin.coinFlip(N); 
+            Console.WriteLine(" Press 1 for Flip Coin and print percentage of Heads and Tails Program \n Press 2 for Leap Year Program");
+            int option = Convert.ToInt32(Console.ReadLine()); //storing  input given by user to choose program
+            switch (option)
+            {
+                case 1: // Flip Coin and print percentage of Heads and Tails Program
+                    Console.WriteLine("Enter the Number of times you wanted to flip the coin");
+                    int N = Convert.ToInt32(Console.ReadLine());
+                    flipCoin.coinFlip(N);
+                    break;
+                case 2: //LEAP YEAR PROGRAM
+                    Console.WriteLine("Enter the year to check it's leap year or not");
+                    int Year = Convert.ToInt32(Console.ReadLine());
+                    if (Year.ToString().Length == 4) // Checking valid year (4 digit)
+                        leapYear.yearLeap(Year);
+                    else
+                        Console.WriteLine("Invalid Input");
+                    break;
+                default:
+                    Console.WriteLine("Please Select Correct input");
+                    break;
+            }
         }
     }
 }
---
+++ BasicCoreProgramsDayFive/leapYear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDayFive
+{
+    internal class leapYear
+    {
+        public static void yearLeap(int Year)
+        {  //if & else if statement for checking year is leap or not
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+                Console.WriteLine("The {0} is leap year", Year);
+            else
+                Console.WriteLine("The {0} is not leap year", Year);
+        }
+    }
+}
